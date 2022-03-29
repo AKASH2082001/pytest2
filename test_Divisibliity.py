2e2f968cdfeb6ac64968606d295811df7a42cfbf
AKASH2082001 <akashbme208@gmail.com>
2022-03-29 00:34:17 -0700

    pytest2

--- test_Divisibliity.py
+++ test_Divisibliity.py
@@ -2,22 +2,23 @@ import Divisibliity
 
 import pytest
 
-def test_Div5():
-    x=12
-    result = Divisibliity.div_5(x)
-    assert result == False
+@pytest.fixture()
+def input():
+    x=50
+    return x
 
-def test_Div7():
-    x=70
-    result = Divisibliity.div_7(x)
+def test_Div5(input):
+    result = Divisibliity.div_5(input)
     assert result == True
 
-def test_Div9():
-    x=90
-    result = Divisibliity.div_9(x)
-    assert result == (x%9 == 0)
-
-def test_Div10():
-    x=3
-    result = Divisibliity.div_10(x)
+def test_Div7(input):
+    result = Divisibliity.div_7(input)
     assert result == False
+
+def test_Div9(input):
+    result = Divisibliity.div_9(input)
+    assert result == False
+
+def test_Div10(input):
+    result = Divisibliity.div_10(input)
+    assert result == True
